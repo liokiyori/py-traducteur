49e835ad0d81cea7e84de513946c49ed29196085
Stephane-ISEN <stephane.jamin-normand@isen-ouest.yncrea.fr>
2024-07-14 17:59:52 +0200

    MAJ des paramètres de l'API

--- api_traducteur/src/config/parametres.py
+++ api_traducteur/src/config/parametres.py
@@ -3,13 +3,6 @@ import os
 # paramètres pour le traducteur
 VERSIONS = ["fr >> en","en >> fr"]
 
-#paramètres pour la bdd
-# BDD_USER = 'root'
-# BDD_PASSWORD = 'example'
-# BDD_HOST = 'localhost'
-# BDD_PORT = "3307"
-# BDD_DATABASE = 'traducteur'
-
 BDD_HOST = os.getenv('DATABASE_HOST', 'localhost')
 BDD_PORT = os.getenv('DATABASE_PORT', 3307)
 BDD_USER = os.getenv('DATABASE_USER', 'traducteur')
