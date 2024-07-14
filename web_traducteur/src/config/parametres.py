31799196318159ccdee122f1c32df9d6056590ce
Stephane-ISEN <stephane.jamin-normand@isen-ouest.yncrea.fr>
2024-07-14 14:11:58 +0200

    Initial commit of bdd, api, and client projects

---
+++ web_traducteur/src/config/parametres.py
@@ -0,0 +1,5 @@
+
+URL_VERSIONS = "http://127.0.0.1:8080/versions"
+URL_TRADUCTEUR = "http://127.0.0.1:8080/traductions"
+URL_TRADUCTIONS = "http://127.0.0.1:8080/traductions/auteur/"
+URL_LOGIN = "http://127.0.0.1:8080/login"
